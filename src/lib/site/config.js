16ecbd283f70d19db95265b3c22151e4346a5af0
DuncanAJohnson <duncanjohnson99@gmail.com>
2026-09-23 21:26:35 -0400

    chemistry version of math figures

---
+++ src/lib/site/config.js
@@ -0,0 +1,5 @@
+// Facts about the site as a whole.
+
+export const SITE_NAME = 'Chemistry Figures'
+export const SITE_URL = 'https://chemistryfigures.com'
+export const SUPPORT_EMAIL = 'support@teacher.dev'
